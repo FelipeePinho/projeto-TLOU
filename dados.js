5cd52e020beefca3539e77ed4cb9e57003b02d0a
Felipe Pinho <86444516+FelipeePinho@users.noreply.github.com>
2024-09-05 17:06:28 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Joel Miller",
+        descricao : "Forjado pelo trauma da perda de sua filha no início do apocalipse, Joel se tornou um homem endurecido e pragmático. Sua jornada ao lado da jovem Ellie, imune ao fungo que assola a humanidade, o coloca em uma encruzilhada moral que o obriga a confrontar seus próprios demônios e a natureza da esperança em um mundo devastado.",
+        link : "https://thelastofus.fandom.com/pt-br/wiki/Joel_Miller",
+        imagem : "https://static.wikia.nocookie.net/thelastofus/images/7/7b/Joel_music_store_close_up.png"
+    },
+    {
+        titulo: "Ellie Williams",
+        descricao: "Ellie Williams é uma jovem imune ao cordyceps, vírus que transformou a humanidade em criaturas infectadas. Cresceu em um mundo pós-apocalíptico e desenvolveu habilidades de sobrevivência impressionantes. Sua jornada ao lado de Joel a força a enfrentar desafios e traumas que a moldam como pessoa.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Ellie",
+        imagem : "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/UIRjzDjQKrOD4kx6ubmlOsrA.png"
+    },
+    {
+        titulo: "Tommy Miller",
+        descricao: "Tommy Miller é o irmão mais novo de Joel e um sobrevivente experiente. Após a perda de sua esposa, ele se dedica a ajudar outros sobreviventes e a construir um futuro melhor. Seu reencontro com Joel e Ellie marca um ponto de virada em suas vidas.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Tommy_Miller",
+        imagem : "https://static.wikia.nocookie.net/thelastofus/images/f/f0/Part_II_Tommy_infobox.png/"
+    }
+];
+
