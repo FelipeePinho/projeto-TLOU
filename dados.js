a923b5d566a32c3a26936cae4ccf2f7908f49d61
Felipe Pinho <86444516+FelipeePinho@users.noreply.github.com>
2024-09-07 23:18:04 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -19,6 +19,48 @@ let dados = [
         link: "https://thelastofus.fandom.com/pt-br/wiki/Tommy_Miller",
         imagem: "https://static.wikia.nocookie.net/thelastofus/images/f/f0/Part_II_Tommy_infobox.png/",
         tags:"irmãozinho irmaozinho tio tommy"
-    }
+    },
+    {
+        titulo: "Tess",
+        descricao: "Parceira de Joel em Boston, Tess é uma sobrevivente dura e experiente. Leal e protetora, ela ajuda Joel a cuidar de Ellie, mas também guarda segredos de seu passado. Sua morte marca um ponto de virada na jornada de Joel.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Tess",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/1/11/8270928204_a0d9857309_z.jpg",
+        tags: "boston contrabandista leal protetora"
+      },
+      {
+        titulo: "Marlene",
+        descricao: "Líder dos Vaga-Lumes, Marlene é uma figura complexa e determinada. Ela acredita que Ellie pode ser a chave para salvar a humanidade e está disposta a fazer qualquer coisa para alcançar seu objetivo, mesmo que isso signifique colocar Joel e Ellie em perigo.",
+        link: "https://thelastofus.fandom.com/wiki/Marlene",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/d/d4/Part_I_Marlene_infobox.png",
+        tags: "vaga-lumes líder determinada sacrificio"
+      },
+      {
+        titulo: "Abby Anderson",
+        descricao: "Uma soldado durona e experiente, Abby é a protagonista de The Last of Us Part II. Buscando vingança pela morte de seu pai, ela inicia uma jornada que a levará a confrontar Joel e Ellie. Apesar de sua busca por vingança, Abby também demonstra compaixão e vulnerabilidade.",
+        link: "https://thelastofus.fandom.com/wiki/Abby_Anderson",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/8/8b/Abby_TLOU2_remaster.PNG/",
+        tags: "vingança soldado forte vulnerável washington"
+      },
+      {
+        titulo: "Lev",
+        descricao: "Um jovem órfão que se junta a Abby em sua jornada. Lev é um devoto de uma seita religiosa e busca um lugar onde possa viver livremente. Sua inocência e fé contrastam com a brutalidade do mundo pós-apocalíptico.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Lev",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/a/a3/Part_II_Lev_infobox.png",
+        tags: "órfão religioso inocente fé"
+      },    
+      {
+        titulo: "Bill",
+        descricao: "Um sobrevivente solitário e paranoico que vive em uma cidade fortificada. Bill é um especialista em eletrônica e armadilhas, e oferece abrigo a Joel e Ellie durante sua jornada. Sua história e relacionamento com Frank são explorados em mais detalhes em The Last of Us Part II.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Bill",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/2/2f/Part_I_Bill_infobox.png",
+        tags: "solitário paranoico engenheiro"
+      },
+      {
+        titulo: "Dina",
+        descricao: "Uma jovem habilidosa com um arco e flecha, Dina é uma sobrevivente durona e independente que vive em Jackson. Ela forma um forte vínculo com Ellie, oferecendo-lhe companheirismo e apoio em um mundo hostil. Sua lealdade e coragem a tornam uma aliada valiosa.",
+        link: "https://thelastofus.fandom.com/pt-br/wiki/Dina",
+        imagem: "https://static.wikia.nocookie.net/thelastofus/images/f/f4/Part_II_Dina_infobox.png/",
+        tags: "jackson arco e flecha habilidosa leal companheira"
+      },
 ];
 
